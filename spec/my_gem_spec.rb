c83943f9936b3987ba8a9fe51545db5e58cf7b00
jaglez <jaglez@ull.edu.es>
2015-11-25 19:34:56 +0000

    Faltan pruebas

--- spec/my_gem_spec.rb
+++ spec/my_gem_spec.rb
@@ -6,33 +6,33 @@ describe MyGem do
     expect(MyGem::VERSION).not_to be nil
   end
 
-  context "# almacenamiento de las coordenadas" do
-    it "Se almacena correctamente la coordenada X" do
-      p1 = Point::Point.new(1,1)
-      expect(p1.x).to eq(1)
-    end
-    it "Se almacena correctamente la coordenada Y" do
-      p1 = Point::Point.new(1,1)
-      expect(p1.y).to eq(1)
-    end
-  end # context
+#  context "# almacenamiento de las coordenadas" do
+#    it "Se almacena correctamente la coordenada X" do
+#      p1 = Point::Point.new(1,1)
+#      expect(p1.x).to eq(1)
+#    end
+#    it "Se almacena correctamente la coordenada Y" do
+#      p1 = Point::Point.new(1,1)
+#      expect(p1.y).to eq(1)
+#    end
+#  end # context
 
-  context "# producto por un escalar" do
-    before :each do
-      @p1 = Point::Point.new(1,1)
-    end    
+#  context "# producto por un escalar" do
+#    before :each do
+#      @p1 = Point::Point.new(1,1)
+#    end    
     
-    it "la coordenada x y la coordenada y se multiplican por el valor" do
-      p2 = @p1*5 
-      expect(p2.x).to eq(5)
-      expect(p2.y).to eq(5)
-    end
-    it "Si se multiplica por -1 se obtiene el opuesto" do
-      p2 = @p1*-1 
-      expect(p2.x).to eq(-1)
-      expect(p2.y).to eq(-1)
-    end
-  end
+#    it "la coordenada x y la coordenada y se multiplican por el valor" do
+#      p2 = @p1*5 
+#      expect(p2.x).to eq(5)
+#      expect(p2.y).to eq(5)
+#    end
+#    it "Si se multiplica por -1 se obtiene el opuesto" do
+#      p2 = @p1*-1 
+#      expect(p2.x).to eq(-1)
+#      expect(p2.y).to eq(-1)
+#    end
+#  end
   
   context "# opuesto de un punto" do
     it "la coordenada x y la coordenada y se multiplican por menos uno" do
